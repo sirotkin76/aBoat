f2c7491add57ab285460d62ad684743d27ff0684
72gsir <72gsir@gmail.com>
2018-01-27 17:41:10 +0300

    a boat

---
+++ Assets/Scripts/KeyManagerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeyManagerController : MonoBehaviour {
+
+	public KeyCode exitGame;
+
+	private Text text;
+
+	void Start() {
+		text = this.GetComponentInChildren<Text>();
+		text.text = "Переключиться - Е; " + " Выход - " + exitGame.ToString() + "; ";
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(exitGame))
+			Application.Quit();
+
+	}
+}
---
+++ Assets/Scripts/SwitchMode.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchMode : MonoBehaviour {
+
+	public GameObject boat;
+	public GameObject boatCamera;
+	public GameObject player;
+	public GameObject playerStartPosition;
+
+	bool switches = true;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown(KeyCode.E) && switches) {
+			switches = !switches;
+			// set to boat mode //
+			boat.GetComponent<Rigidbody>().isKinematic = false;
+			boat.GetComponent<BoatController>().enabled = true;
+			boatCamera.SetActive(true);
+
+			player.SetActive(false);
+		} else if (Input.GetKeyDown(KeyCode.E) && !switches) {
+			switches = !switches;
+			// set to FPS mode //
+			boat.GetComponent<Rigidbody>().isKinematic = true;
+			boat.GetComponent<BoatController>().enabled = false;
+			boatCamera.SetActive(false);
+
+			player.SetActive(true);
+			player.transform.position = playerStartPosition.transform.position;
+		}
+
+	}
+}
